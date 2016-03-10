a50f868c64a26a03973d3e51f42afb5209485e02
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-03-11 00:38:24 +0100

    no message

--- prompt.zsh
+++ prompt.zsh
@@ -3,12 +3,6 @@ autoload -U colors && colors
 
 LC_ALL="en_US.UTF-8"
 
-function echo_color {
-  local color=$1
-  local text=$2
-  echo -n "$color""$text""$reset_color"
-}
-
 local CMD_ID=0
 function preexec {
   CMD_ID=$(expr $CMD_ID + 1)
@@ -20,33 +14,27 @@ function precmd {
   exit_code=$?
   if [ $exit_code -ne 0 ] && [ $FAILD_CMD_ID -ne $CMD_ID ]; then
     FAILD_CMD_ID=$CMD_ID
-    echo_color $fg_bold[red] "✖ $exit_code"
-    echo
+    echo "${fg_bold[red]}✖ $exit_code$reset_color"
   fi
 
+  current_user=$(whoami)
+  current_host=$(hostname -s)
   current_dir=$(pwd | sed -e "s|^$HOME|~|" -e 's-\([^/.]\)[^/]*/-\1/-g')
 
-  echo_color $fg_bold[blue] "# "
+  precmd="${fg_bold[blue]}#$reset_color "
   if [ "$USER" = "root" ]; then
-    echo_color $fg_bold[red] "$(whoami)"
+    precmd+="${fg_bold[red]}$current_user$reset_color"
   else
-    echo_color $fg[cyan] "$(whoami)"
+    precmd+="${fg[cyan]}$current_user$reset_color"
   fi;
-  echo_color $fg[cyan] " @ "
-  echo_color $fg[green] "$(hostname -s)"
-  echo_color $fg[white] " in "
-  echo_color $fg[yellow] "$current_dir "
-  #echo_color $fg[white] " GIT_INFO "
-  #echo_color $fg[white] "$(date '+%Y-%m-%d %H:%M:%S')"
-  echo
-  echo_color $fg_bold[white] "❯ "
+  precmd+="${fg[white]}@$reset_color${fg[green]}$current_host$reset_color"
+  precmd+=" ${fg[white]}in$reset_color ${fg[yellow]}$current_dir$reset_color"
+  echo "$precmd"
 }
 
 #setopt promptsubst
 PS1='❯ '
-#PS1='$(precmd2)'
 PS2='▪ '
-#PS2='└ '
 
 #RPROMPT='[%D{%H:%M:%S}]'
 
