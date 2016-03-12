f8048e9acc93c122383a1b6d095c990e089ed57a
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-03-12 01:23:29 +0100

    no message

--- aliases.zsh
+++ aliases.zsh
@@ -7,7 +7,6 @@ alias http-server-ssl='http-server --ssl --cert /.ssl/localhost.pem --key /.ssl/
 
 #alias simple_http_server='open http://localhost:8000; python -m SimpleHTTPServer'
 
-
 ##### suffix aliases
 
 #alias -s jpeg="open"
---
+++ functions/network.zsh
@@ -0,0 +1,23 @@
+function ip-internal {
+  if [ -n "$1" ]; then
+    interface=$1
+    ip=$(ipconfig getifaddr $interface)
+    if [ $? -eq 0 ]; then
+      echo $ip
+    else
+      return 1
+    fi
+  else
+    interface_list=($(ifconfig -l))
+    for interface in $interface_list; do
+        ip=$(ipconfig getifaddr $interface)
+        if [ -n "$ip" ]; then
+          echo "$interface: $ip"
+        fi
+    done
+  fi
+}
+
+function ip-external {
+  curl ifconfig.me
+}
