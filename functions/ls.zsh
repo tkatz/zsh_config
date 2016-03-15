8f10aaceacaf79a54d213363882146887b53ddb3
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-03-15 16:25:07 +0100

    no message

--- functions/ls.zsh
+++ functions/ls.zsh
@@ -1,3 +1,11 @@
 function ls_find {
-  ls -FGl -d $(find $@ )
+  # if [[ "$@" != *" - "* ]]; then
+  #   find_args="$@"
+  # else
+  #   ls_args=$(echo "$@" | sed -e 's/\(.*\) - \(.*\)/\1/')
+  #   find_args=$(echo "$@" | sed -e 's/\(.*\) - \(.*\)/\2/')
+  # fi
+  #
+  # ls $ls_args -d $(find $find_args )
+  ls -d $(find $@ )
 }
