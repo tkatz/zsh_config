0e66cae5759fdad2b4802b87da95138d27d00e80
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-03-17 00:30:18 +0100

    no message

--- functions/git.zsh
+++ functions/git.zsh
@@ -1,1 +1,3 @@
-
+function git-log {
+  git log --graph --all --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%ad%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n           %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'
+}
