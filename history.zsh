a752a5ea664af9a8fab8e5e3d6b31ab47e5a5f24
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-03-31 17:16:10 +0200

    no message

--- history.zsh
+++ history.zsh
@@ -8,15 +8,14 @@ setopt INC_APPEND_HISTORY       # append to history immediately.
 
 
 setopt HIST_EXPIRE_DUPS_FIRST   # allow dups, but expire old ones when I hit HISTSIZE
-#setopt HIST_IGNORE_DUPS        # ignore duplication command history list
+#setopt HIST_IGNORE_DUPS        # ignore consecutive duplication in command history list
+setopt HIST_IGNORE_ALL_DUPS     # ignore all duplication in command history list
 setopt HIST_FIND_NO_DUPS
 setopt HIST_IGNORE_SPACE
 setopt HIST_VERIFY
 #setopt SHARE_HISTORY # share command history between sessions
 
-
 setopt NO_HIST_BEEP
-
 
 # HH configuration
 export HISTFILE=$HISTFILE       # ensure history file visibility
