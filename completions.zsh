e28152096f7d9bdf282799b175e696f01f416477
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-04-04 11:15:59 +0200

    no message

--- completions.zsh
+++ completions.zsh
@@ -2,12 +2,14 @@
 
 #setopt AUTO_CD
 setopt EXTENDEDGLOB
+#setopt GLOB_COMPLETE
 setopt NOMATCH # If a pattern for filename generation has no matches, print an error, instead of leaving it unchanged in the argument list.
 setopt COMPLETE_IN_WORD         # Not just at the end
 setopt ALWAYS_TO_END            # When complete from middle, move cursor
 setopt AUTO_LIST
 setopt AUTO_MENU
 setopt MENU_COMPLETE
+
 
 unsetopt CORRECT # disable Spelling corrections
 
@@ -16,20 +18,19 @@ autoload -Uz compinit && compinit
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30"
 
-zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|=* r:|=*'
+zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*:*:*:*:*' force-list always
+zstyle ':completion:*:*:*:*:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*' # Hypen- & caseinsensitive matching
+
 zstyle ':completion:*' menu select
 zstyle ':completion:*' list-colors "$LS_COLORS"
 zstyle ':completion:*' completer _complete
 
 zstyle ':completion:*:*:*:*:processes' command 'ps -u ${USER} -o pid,user,command'
-zstyle ':completion:*:*:*:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:*:*:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-_]#)*=0=01;34=02=0'
 zstyle ':completion:*:*:*:*:processes-names' command  'ps -c -u ${USER} -o command | uniq'
 
 zstyle ':completion:*:killall:*' command 'ps -u $USER -o command'
-
-zstyle ':completion:*:*:kill:*' force-list always
-zstyle ':completion:*:*:kill:*' list-colors '=(#b) #([0-9]#)*=0=01;31'
-zstyle ':completion:*:*:kill:*' menu yes select
 
 ###
 
