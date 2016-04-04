32751148a511132a15b48a665b9d770c5c59280f
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-04-04 11:45:25 +0200

    no message

--- completions.zsh
+++ completions.zsh
@@ -18,13 +18,12 @@ autoload -Uz compinit && compinit
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30"
 
+zstyle ':completion:*' completer _complete
+zstyle ':completion:*' list-colors "$LS_COLORS"
+
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:*:*:*' force-list always
 zstyle ':completion:*:*:*:*:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*' # Hypen- & caseinsensitive matching
-
-zstyle ':completion:*' menu select
-zstyle ':completion:*' list-colors "$LS_COLORS"
-zstyle ':completion:*' completer _complete
 
 zstyle ':completion:*:*:*:*:processes' command 'ps -u ${USER} -o pid,user,command'
 zstyle ':completion:*:*:*:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-_]#)*=0=01;34=02=0'
@@ -40,9 +39,7 @@ zstyle ':completion:*:killall:*' command 'ps -u $USER -o command'
 # zstyle ':completion:*' group-name ''
 # zstyle ':completion:*' menu select=2 eval "$(dircolors -b)"
 # zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-# zstyle ':completion:*' list-colors ''
-# zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-# zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+
 # zstyle ':completion:*' menu select=long
 # zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
 # zstyle ':completion:*' use-compctl false
