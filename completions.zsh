3cdf14c8483c74b916a1fd7ca6f3d2e05e180fa9
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-04-04 23:00:30 +0200

    no message

--- completions.zsh
+++ completions.zsh
@@ -18,15 +18,24 @@ autoload -Uz compinit && compinit
 LSCOLORS="exfxcxdxbxegedabagacad"
 LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30"
 
-zstyle ':completion:*' completer _complete
+
+# Fuzzy matching of completions
+zstyle ':completion:*' completer _complete _match _approximate _history
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)' # the number of allowed errors by _approximate increases with the length of what you have typed so far
+
 zstyle ':completion:*' list-colors "$LS_COLORS"
 
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:*:*:*' force-list always
 zstyle ':completion:*:*:*:*:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*' # Hypen- & caseinsensitive matching
 
+zstyle ':completion:*:cd:*' ignore-parents parent pwd # cd will never select the parent directory (e.g.: cd ../<TAB>)
+
 zstyle ':completion:*:*:*:*:processes' command 'ps -u ${USER} -o pid,user,command'
 zstyle ':completion:*:*:*:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-_]#)*=0=01;34=02=0'
+
 zstyle ':completion:*:*:*:*:processes-names' command  'ps -c -u ${USER} -o command | uniq'
 
 zstyle ':completion:*:killall:*' command 'ps -u $USER -o command'
