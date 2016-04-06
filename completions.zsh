c04507ac8e6a169ecb59fea895edc80bbb082ce0
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-04-06 11:03:42 +0200

    no message

--- completions.zsh
+++ completions.zsh
@@ -1,22 +1,22 @@
 # Completions ##################################################################
 
-#setopt AUTO_CD
+# setopt AUTO_CD
 setopt EXTENDEDGLOB
-#setopt GLOB_COMPLETE
+# setopt GLOB_COMPLETE
 setopt NOMATCH # If a pattern for filename generation has no matches, print an error, instead of leaving it unchanged in the argument list.
 setopt COMPLETE_IN_WORD # Not just at the end
 setopt ALWAYS_TO_END # When complete from middle, move cursor
 setopt AUTO_LIST # Automatically list choices on ambiguous completion.
 setopt AUTO_MENU # Show completion menu on a successive tab press.
-setopt MENU_COMPLETE # Do not autoselect the first completion entry.
+# setopt MENU_COMPLETE # Do autoselect the first completion entry.
 
 unsetopt CORRECT # disable Spelling corrections
 
 autoload -Uz compinit && compinit
 
 # ls Colors
-LSCOLORS="exfxcxdxbxegedabagacad"
-LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30"
+export LSCOLORS="exfxcxdxbxegedabagacad" #used by ls
+export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30"
 
 
 # Fuzzy matching of completions
@@ -25,10 +25,13 @@ zstyle ':completion:*:match:*' original only
 zstyle ':completion:*:approximate:*' max-errors 1 numeric
 zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)' # the number of allowed errors by _approximate increases with the length of what you have typed so far
 
+
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*' # Hypen- & caseinsensitive matching
 zstyle ':completion:*' list-colors "$LS_COLORS"
 
 zstyle ':completion:*:*:*:*:*' menu select
-zstyle ':completion:*:*:*:*:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*' # Hypen- & caseinsensitive matching
+
+
 
 zstyle ':completion:*:cd:*' ignore-parents parent pwd # cd will never select the parent directory (e.g.: cd ../<TAB>)
 
