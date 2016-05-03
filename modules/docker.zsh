27308f6808210d883fa8195ec5ca7c6ba6c95ece
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-05-03 17:56:09 +0200

    no message

--- modules/docker.zsh
+++ modules/docker.zsh
@@ -1,5 +1,11 @@
 function docker-setup {
   DOCKER_ENV=${1-default}
+
+  DOCKER_MACHINE_STATUS=$(docker-machine status $DOCKER_ENV)
+  if [ "$DOCKER_MACHINE_STATUS" = "Stopped" ]; then
+    docker-machine start $DOCKER_ENV
+  fi
+
   eval $(docker-machine env $DOCKER_ENV)
 }
 
