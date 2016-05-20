8276ad3aafafb92488ded2c2390fb4fb8d36ae0e
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-05-20 17:57:50 +0200

    no message

--- modules/git.zsh
+++ modules/git.zsh
@@ -9,5 +9,47 @@ git config --global pull.rebase true
 # git config --global commit.template ~/git_commit_template.txt
 
 # npm install --global commitizen
-# npm install --global standard-version
+# npm install --global cz-conventional-changelog
+# echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
+# npm install --global conventional-changelog-cli
 # npm install --global conventional-recommended-bump
+
+function git-repository-version-latest {
+  git describe --all --tags --match 'v*' --first-parent --abbrev=0 | sed 's|^v||'
+}
+
+function git-repository-version-next {
+  latest_version=$(git-repository-version-latest)
+  latest_version_array=($(echo "$latest_version" | grep -E -o "[^.-]+" ))
+  latest_version_array[4]=$(echo "$latest_version" | sed "s|^[^-]*||" )
+
+  next_version_array=($latest_version_array)
+  if [ -n "$(git log --grep '^BREAKING CHANGE')" ]; then
+    next_version_array[1]=$((${next_version_array[1]}+1))
+    next_version_array[2]=0
+    next_version_array[3]=0
+  elif [ -n "$(git log --grep '^feat')" ]; then
+    next_version_array[2]=$((${next_version_array[2]}+1))
+    next_version_array[3]=0
+  else
+    next_version_array[3]=$((${next_version_array[3]}+1))
+  fi
+
+  next_version="${next_version_array[1]}.${next_version_array[2]}.${next_version_array[3]}${next_version_array[4]}"
+  echo "$next_version"
+}
+
+function git-repository-version-compare {
+  current_version=$1
+  current_version_array=($(echo "$current_version" | grep -E -o "[^.-]+" ))
+
+  next_version=$(git-repository-version-next)
+  next_version_array=($(echo "$next_version" | grep -E -o "[^.-]+" ))
+
+  if [ ${current_version_array[1]} -lt ${next_version_array[1]} ] \
+  || [ ${current_version_array[2]} -lt ${next_version_array[2]} ] \
+  || [ ${current_version_array[3]} -lt ${next_version_array[3]} ]; then
+    >&2 echo "version needs to be at least $next_version"
+    return 1
+  fi
+}
