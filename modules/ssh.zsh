a62d8df8a381bde802373565f6f406b63806a466
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-05-29 20:33:45 +0200

    feat: add ssh functions

--- modules/ssh.zsh
+++ modules/ssh.zsh
@@ -7,15 +7,25 @@ function ssh-tunnel {
   ssh -L ${local_port}:localhost:${remote_port} ${remote_host}
 }
 
+# ~/.ssh/config
+######
 # Host *
-#   IdentityFile ~/.ssh/%l_%r@%h_id_rsa
+#   IdentityFile ~/.ssh/id_rsa+%r@%h
+# Host *
+#   IdentityFile ~/.ssh/id_rsa
+######
 #
-# e.g. ~/.ssh/<LOCAL_USER>_<REMOTE_USER>@<REMOTE_HOST>_id_rsa
-
+# e.g. ~/.ssh/id_rsa+<REMOTE_USER>@<REMOTE_HOST>
+#
 # ln -s <KEY_NAME>.pub <TARGET_KEY_NAME>.pub
 # ln -s <KEY_NAME>     <TARGET_KEY_NAME>
 
 
+function ssh-key-set {
+   ssh-add -D
+   ssh-add ~"/.ssh/$1"
+}
+
 function ssh-key-info {
-   ssh-keygen -l -f ~/.ssh/$1
+   ssh-keygen -l -f ~"/.ssh/$1"
 }
